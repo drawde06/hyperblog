6c5fae90c5aa3243b659cc062ca838decd33f18c
ccorzo <cristev00@gmail.com>
2022-09-06 22:30:03 -0500

    add new python script v1

---
+++ python-scripts/suma.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+print ("Programa que suma 2 numeros")
+a = int(input("Introduzca el sumando 1: "))
+b = int(input("Introduzca el sumando 2: "))
+suma = a + b
+print ("El resultado de la suma es: ", suma)
