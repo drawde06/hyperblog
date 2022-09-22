6919f28d81280de6015bbcd09c4b98e7342dec9e
Edward Corzo <drawde06@gmail.com>
2022-09-21 20:31:46 -0500

    Cambie las variales a,b por x,y

--- python-scripts/suma.py
+++ python-scripts/suma.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 print ("Programa que suma 2 numeros")
-a = int(input("Introduzca el sumando 1: "))
-b = int(input("Introduzca el sumando 2: "))
-suma = a + b
+x = int(input("Introduzca el sumando 1: "))
+y = int(input("Introduzca el sumando 2: "))
+suma = x + y
 print ("El resultado de la suma es: ", suma)
